9cdb372eb06c6e7d7205a3b7c2692d087732f4d8
Yann <savuir@gmail.com>
2015-12-06 05:39:22 +0300

    Modify setup.py for pypi Add setup.cfg for pypi

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='blo',
     version='0.1',
     license='BSD-3-clause',
+    author='Yann Savuir',
+    author_email='savuir@gmail.com',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -12,4 +14,7 @@ setup(
     },
     package_data={'': ['blo/default.json']},
     include_package_data=True,
+    url='https://github.com/savuir/blo',  # use the URL to the github repo
+    download_url='https://github.com/savuir/blo/tarball/0.1',  # I'll explain this in a second
+    keywords=['blogging', 'blog', 'static blog generator'],  # arbitrary keywords
 )
