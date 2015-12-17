fee7a23ca05a05a8f46fc61e43fd488e941d0500
Yann Savuir <savuir@gmail.com>
2015-12-18 02:23:53 +0300

    Fix requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    install_requires = f.read().split('\n')
+
 setup(
     name='blo',
-    version='0.1',
+    version='0.3',
     license='BSD-3-clause',
     author='Yann Savuir',
     author_email='savuir@gmail.com',
@@ -12,7 +15,8 @@ setup(
             'blo = blo.blog:main',
         ],
     },
-    package_data={'': ['blo/default.json']},
+    install_requires=install_requires,
+    package_data={'': ['blo/default.json', 'blo/draft_templates.json',]},
     include_package_data=True,
     url='https://github.com/savuir/blo',  # use the URL to the github repo
     download_url='https://github.com/savuir/blo/tarball/0.1',  # I'll explain this in a second
