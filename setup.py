0fce1b7e04d4ec8e9aff8e142308c7a18b64d97b
Yann <savuir@gmail.com>
2016-10-11 19:53:51 +0300

    version 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blo',
-    version='0.4',
+    version='0.5',
     license='BSD-3-clause',
     author='Yann Savuir',
     author_email='savuir@gmail.com',
